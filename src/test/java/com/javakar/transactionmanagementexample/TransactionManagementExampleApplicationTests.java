e938767d8e2b4e3557d8f306fbaef560637f9c73
Aamir shayan <aamirshayanshaikh@gmail.com>
2022-10-29 13:55:56 +0500

    Transaction management spring boot

---
+++ src/test/java/com/javakar/transactionmanagementexample/TransactionManagementExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javakar.transactionmanagementexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionManagementExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
